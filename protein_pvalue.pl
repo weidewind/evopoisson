940d2e3c0fc84e544ce12162fb37615f25f99a81
weidewind <weidewind@gmail.com>
2016-10-11 15:50:13 +0300

    trash deleted

--- protein_pvalue.pl
+++ protein_pvalue.pl
@@ -16,6 +16,9 @@ my $input = '';
 my $output = '';	# option variable with default value
 my $subtract_tallest = '0';
 my $restrictions = '0';
+my $step = 0.5;
+my $simnumber = 10000;
+my $maxmem = 4000000;
 my $verbose;
 
 GetOptions (	'protein=s' => \$protein,
@@ -24,14 +27,39 @@ GetOptions (	'protein=s' => \$protein,
 		'output=s' => \$output,
 		'subtract_tallest=i' => \$subtract_tallest,
 		'restrictions=s' => \$restrictions,
+		'simnumber=i' => \$simnumber,
+		'maxmem=i' => \$maxmem,
+		'step=s' => \$step,
 		'verbose'  => \$verbose,
 	);
 
 unless ($subtract_tallest == 0 || $subtract_tallest == 1) {die "subtract_tallest must be either 0 or 1\n";}
 ## for concat_and_divide_simult you need a mutmap produced from realdata, therefore fromfile => true
 my $args = {bigdatatag => $input, bigtag => $output, protein => $protein, state => $state, subtract_tallest => $subtract_tallest, fromfile => 1}; 
-unless  (Mutmap::realdata_exists($args)) { die "No such realdata!"; }
+
+## Checking if appropriate realdata exists, initializing
 my @restriction_levels = split(/,/, $restrictions);
+my $specified_restriction = List::Util::min(@restriction_levels);
+
+if  (! (Mutmap::realdata_exists($args))) { 
+	print "No realdata exists for specified parameters, going to prepare it.."; 
+	$args->{fromfile} = 0;
+	my $mutmap = Mutmap->new($args);
+	$mutmap-> prepare_real_data ($specified_restriction, $step);
+}
+else {
+	my $rr = Mutmap::check_realdata_restriction($args);
+	if ($rr > $specified_restriction){
+		print "Existing realdata restriction is greater than minimal restriction you specified: ".$rr." > ".$specified_restriction."\nGoing to overwrite realdata..\n"; 
+		$args->{fromfile} = 0;
+		my $mutmap = Mutmap->new($args);
+		$mutmap-> prepare_real_data ($specified_restriction, $step);
+	}
+	else {
+		print "Going to use existing realdata with restriction $rr\n";
+	}
+}
+
 my $rr = Mutmap::check_realdata_restriction($args);
 my $sr = List::Util::min(@restriction_levels);
 print "realdata restriction is $rr\n";
