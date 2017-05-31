6cd9b73edc9187540fdb2dd029ced51207e4d4fa
weidewind <weidewind@gmail.com>
2017-05-31 17:04:35 +0300

    dist matrix test added

--- test_dist_matrix.pl
+++ test_dist_matrix.pl
@@ -6,6 +6,30 @@ use Bio::Phylo::IO;
 use File::Spec;
 use strict;
 
+sub parse_tree {
+					my $tree_file = $_[0];
+					open TREE, "< $tree_file" or die "Cannot open file ".$tree_file."\n";
+					# get a newick string from some source
+ 					my $tree_string;
+ 					my $t;
+ 					while($t = <TREE>){
+ 						$t =~ s/\n$//;
+ 						$tree_string = $tree_string.$t;
+ 					}
+ 					 close TREE;
+
+ 					# Call class method parse from Bio::Phylo::IO
+ 					# note: newick parser returns 'Bio::Phylo::Forest'
+                    # Call ->first to retrieve the first tree of the forest.
+ 					my $tree = Bio::Phylo::IO->parse(
+  					  -string => $tree_string,
+   					  -format => 'newick'
+ 					)->first;
+
+ 					return $tree;
+	
+} 
+
 sub old_distance_matrix {
 	    my $input_base = shift;
 		my $file = File::Spec->catfile($input_base, "h3_distance_matrix.csv");
