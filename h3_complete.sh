c082f064d9bf55060ba5ecbffdab19339f413a86
weidewind <weidewind@gmail.com>
2017-06-13 13:22:39 +0300

    changed cleaving to concat

--- h3_complete.sh
+++ h3_complete.sh
@@ -2,9 +2,15 @@
 
 foldername="ksudata"
 (perl poissonnolim.pl -p h3 --state nsyn --input ksu --restrictions 0.03,0.06,0.09 --simnumber 2000 --mutnum_control 0 --shuffler_type exp --maxmem 250000000 --output $foldername &>output/logs/ksudata_whole ;
+<<<<<<< HEAD
+perl single_sites_nolim.pl -p h3 --state nsyn --input ksu --mutnum_control 0  --restrictions 0.03,0.06,0.09 --output $foldername &>output/logs/ksudata_sites ) &
+perl poissonnolim.pl -p h3 --state syn --input ksu  --restrictions 0.03,0.06,0.09 --simnumber 2000 --mutnum_control 0 --shuffler_type exp --maxmem 250000000 --output $foldername &>output/logs/ksudata_whole_syn
+perl single_sites_nolim.pl -p h3 --state syn --input ksu --restrictions 0.03,0.06,0.09 --mutnum_control 0 --output $foldername &>output/logs/ksudata_sites_syn
+=======
 perl single_sites_nolim.pl -p h3 --state nsyn input ksu --mutnum_control 0  --restrictions 0.03,0.06,0.09 --output $foldername &>output/logs/ksudata_sites ) &
 perl poissonnolim.pl -p h3 --state syn --input ksu  --restrictions 0.03,0.06,0.09 --simnumber 2000 --mutnum_control 0 --shuffler_type exp --maxmem 250000000 --output $foldername &>output/logs/ksudata_whole_syn
 perl single_sites_nolim.pl -p h3 --state syn input ksu --restrictions 0.03,0.06,0.09 --mutnum_control 0 --output $foldername &>output/logs/ksudata_sites_syn
+>>>>>>> 899289bd3db814ed9bee7d529aa109571234ac71
 		
 perl grep_results.pl -i output/${foldername}/nsyn/maxpath_not_subtracted
 perl grep_results.pl -i output/${foldername}/syn/maxpath_not_subtracted
